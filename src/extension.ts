224836c661ec8fb20d23fca4e6b222dece4195b0
Dennis Müller <d.mueller@kwarc.info>
2022-06-02 21:39:12 +0200

    update

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,10 @@ import { launchLocal, launchRemote } from './nonweb/launches';
 import { STeXContext } from './shared/context';
 import { getJarpath, getMathHub, setup } from './nonweb/setup';
 
+let stexc : STeXContext;
+
 export function activate(context: vscode.ExtensionContext) {
-	let stexc = new STeXContext(context);
+	stexc = new STeXContext(context);
 	registerCommands(stexc);
 	if (getMathHub() && getJarpath()) {
 		launchLocal(stexc);
@@ -16,4 +18,8 @@ export function activate(context: vscode.ExtensionContext) {
 	//launchRemote(stexc);
 }
 
-export function deactivate() {}
+export function deactivate() {
+	if (stexc.client) {
+		stexc.client.stop();
+	}
+}
--- src/shared/context.ts
+++ src/shared/context.ts
@@ -2,9 +2,19 @@ import * as weblanguage from 'vscode-languageclient/browser';
 import * as language from 'vscode-languageclient/node';
 import * as vscode from 'vscode';
 
+let outputChannel : vscode.OutputChannel;
+function getoutputChannel() {
+	if (outputChannel) {
+		return outputChannel;
+	} else {
+		outputChannel = vscode.window.createOutputChannel("sTeX");
+		return outputChannel;
+	}
+}
+
 export class STeXContext {
 	vsc: vscode.ExtensionContext;
-	outputChannel = vscode.window.createOutputChannel("sTeX");
+	outputChannel = getoutputChannel();
 	client? : language.LanguageClient | weblanguage.LanguageClient;
 	constructor(context: vscode.ExtensionContext) {
 		this.vsc = context;
