af93265bf03bca8ba6fec323568c6d950a400ad8
Philipp Hutterer <dev.philipphutterer@gmail.com>
2022-07-27 12:52:17 +0200

    feat: prefer single (lazy) outputChannel in client if possible

--- src/shared/context.ts
+++ src/shared/context.ts
@@ -14,11 +14,18 @@ function getoutputChannel() {
 }
 
 export class STeXContext {
+	private _outputChannel: vscode.OutputChannel | undefined;
 	vsc: vscode.ExtensionContext;
-	outputChannel = getoutputChannel();
 	client? : language.LanguageClient | weblanguage.LanguageClient;
 	constructor(context: vscode.ExtensionContext) {
 		this.vsc = context;
 	}
 	mathhub? : MathHubTreeProvider;
+
+	get outputChannel(): vscode.OutputChannel {
+		if(this._outputChannel === undefined) {
+			this._outputChannel = this.client?.outputChannel || getoutputChannel();
+		}
+		return this._outputChannel;
+	}
 }
